143100c47ae99e8df921686b716575c70cfc65a9
Korolev A.N <korolev.a.n@sblogistica.ru>
2024-12-02 14:08:51 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,265 @@
+// main.go
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/segmentio/fasthash/fnv1a"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+type tline struct {
+	count, summ, max, avg int
+	keys                  []string
+	id                    string
+}
+
+var (
+	aggr, group, format string
+	files, groupField   []string
+)
+
+func processFile(filePath string, wg *sync.WaitGroup, results chan map[string]tline) {
+
+	defer wg.Done()
+
+	var isMultiLine bool
+	var line string
+	var lines map[string]tline
+
+	isMultiLine = false
+	line = ""
+
+	//fmt.Printf("FilePath: %s\n", filePath)
+
+	//ata, err := os.ReadFile(filePath)
+
+	file, err := os.Open(filePath) // Открываем файл
+
+	if err != nil {
+		fmt.Printf("Ошибка чтения файла %s: %v\n", filePath, err)
+		return
+	}
+	defer file.Close() // Закрываем файл в конце функции
+
+	scanner := bufio.NewScanner(file) // Создаем сканер для чтения файла построчно
+
+	linedata := &tline{
+		keys:  []string{},
+		count: 1,
+	}
+
+	lines = make(map[string]tline)
+
+	for scanner.Scan() {
+
+		var intVal int
+
+		if isMultiLine {
+			line = line + "\n" + scanner.Text()
+		} else {
+			line = scanner.Text()
+			line = strings.Replace(line, "\ufeff", "", 1)
+		}
+
+		// Используем форматирование из format.go
+		formatter := &formatter1C{}
+		//formatter := new(formatter1C)
+
+		parsedData, err := formatter.Format(line)
+
+		if err != nil {
+			isMultiLine = true
+			continue
+		}
+
+		if isMultiLine {
+			isMultiLine = false
+		}
+
+		if len(groupField) > 0 {
+			for _, field := range groupField {
+				linedata.keys = append(linedata.keys, parsedData[field])
+			}
+		} else {
+			linedata.keys = append(linedata.keys, line)
+		}
+
+		if aggr != "" {
+			intVal, _ = strconv.Atoi(parsedData[aggr])
+		}
+
+		key := getHash(strings.Join(linedata.keys, "-"))
+		linedata.id = key
+
+		if _, exists := lines[key]; exists {
+			// Получаем текущее значение строки по ключу
+			currentLine := lines[key]
+			currentLine.count++
+			currentLine.max = int(math.Max(float64(intVal), float64(currentLine.max)))
+			currentLine.summ += intVal
+			// Обновляем значение в мапе
+			lines[key] = currentLine
+		} else {
+			// Инициализируем новую запись в мапе, если ключ не существует
+			lines[key] = tline{
+				keys:  linedata.keys,
+				count: 1,
+				max:   intVal,
+				summ:  intVal,
+			}
+		}
+
+		linedata = &tline{
+			keys:  []string{},
+			count: 1,
+		}
+	}
+
+	results <- lines
+}
+
+func init() {
+
+	kingpin.Flag("group", "Имена свойств для по которым нужно группировать").Short('g').Default("event").StringVar(&group)
+	kingpin.Flag("aggregate", "Имя свойства для агрегации").Short('a').Default("duration").StringVar(&aggr)
+	kingpin.Flag("format", "Формат вывода: csv или json").Short('o').Default("csv").StringVar(&format)
+
+	kingpin.Arg("files", "Файлы тех журнала *.log").Required().StringsVar(&files)
+
+	runtime.SetMutexProfileFraction(5)
+}
+
+func (line tline) MarshalJSON() ([]byte, error) {
+	var lineMap = make(map[string]any)
+
+	lineMap["Count"] = line.count
+	lineMap["Avg"] = line.avg
+	lineMap["Max"] = line.max
+	lineMap["Summ"] = line.summ
+
+	if len(groupField) > 0 {
+		for key, value := range groupField {
+			lineMap[value] = line.keys[key]
+		}
+	}
+
+	return json.Marshal(&lineMap)
+}
+
+func exportJSON(w io.Writer, results map[string]tline) {
+	rows := make([]tline, 0, len(results))
+
+	for _, value := range results {
+		rows = append(rows, value)
+	}
+
+	json.NewEncoder(w).Encode(rows)
+}
+
+func exportCSV(w io.Writer, results map[string]tline) {
+
+	fmt.Fprintf(w, "Count,Summ,Max") // Записываем заголовки
+
+	if len(groupField) > 0 {
+		for _, field := range groupField {
+			fmt.Print(",", field)
+		}
+	}
+	fmt.Fprintln(w)
+
+	for _, value := range results {
+		fmt.Fprintf(w, "%d,%d,%d", value.count, value.summ, value.max)
+
+		if len(groupField) > 0 {
+			for key, _ := range groupField {
+				if strings.Contains(value.keys[key], "\n") {
+					fmt.Fprint(w, ",'", value.keys[key], "'")
+				} else {
+					fmt.Fprint(w, ",", value.keys[key])
+				}
+			}
+		}
+
+		fmt.Fprintln(w)
+	}
+}
+
+func main() {
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	var filesTJ []string
+
+	for _, mask := range files {
+		findFiles, err := filepath.Glob(mask)
+		if err != nil { // Проверка на ошибку
+			log.Printf("Ошибка при поиске файлов: %v\n", err)
+			continue
+		}
+		filesTJ = append(filesTJ, findFiles...)
+	}
+
+	if len(filesTJ) == 0 {
+		log.Fatal("Нет файлов для анализа")
+		os.Exit(1)
+	}
+
+	groupField = strings.Split(group, ",")
+
+	results := make(chan map[string]tline, len(filesTJ))
+
+	var wg sync.WaitGroup
+
+	for _, filePath := range filesTJ {
+		wg.Add(1)
+		go processFile(filePath, &wg, results)
+	}
+
+	wg.Wait()
+
+	close(results)
+
+	allResults := make(map[string]tline)
+
+	for elem := range results {
+		for key, value := range elem {
+
+			if _, exists := allResults[key]; exists {
+				currentLine := allResults[key]
+				currentLine.count = currentLine.count + value.count
+				currentLine.max = int(math.Max(float64(value.max), float64(currentLine.max)))
+				currentLine.summ += value.summ
+				// Обновляем значение в мапе
+				allResults[key] = currentLine
+			} else {
+				allResults[key] = value
+			}
+		}
+	}
+
+	if format == "csv" {
+		// Сохраняем allResults в CSV в stdout
+		exportCSV(os.Stdout, allResults)
+	} else if format == "json" {
+		exportJSON(os.Stdout, allResults)
+	}
+}
+
+func getHash(inStr string) string {
+	//Sum := md5.Sum([]byte(inStr))
+	//return fmt.Sprintf("%x", Sum)
+	Sum := fnv1a.HashString64(inStr)
+	return fmt.Sprintf("%x", Sum)
+}
---
+++ main_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestExportJSON(t *testing.T) {
+	results := map[string]tline{
+		"key1": {count: 1, summ: 10, max: 10, avg: 10, keys: []string{"DBMSSQL"}},
+		"key2": {count: 2, summ: 20, max: 15, avg: 10, keys: []string{"DBMSSQL", "SELECT 1"}},
+		"key3": {count: 2, summ: 20, max: 15, avg: 10, keys: []string{"DBMSSQL"}},
+	}
+
+	group = "event"
+	groupField = strings.Split(group, ",")
+
+	var buf bytes.Buffer
+
+	exportJSON(&buf, results)
+
+	var actual []map[string]interface{}
+	json.Unmarshal(buf.Bytes(), &actual)
+
+	if len(actual) != 3 {
+		t.Errorf("Ожидалось 3 элемента, получено: %d", len(actual))
+	}
+
+	for i, item := range actual {
+		//fmt.Println("key" + strconv.Itoa(i+1))
+		etalon := results["key"+strconv.Itoa(i+1)]
+
+		if item["Count"] != float64(etalon.count) {
+			t.Errorf("Ожидалось: %d, получено: %v", etalon.count, item["Count"])
+		}
+		if item["Summ"] != float64(etalon.summ) {
+			t.Errorf("Ожидалось: %d, получено: %v", etalon.summ, item["Summ"])
+		}
+		if item["Max"] != float64(etalon.max) {
+			t.Errorf("Ожидалось: %d, получено: %v", etalon.max, item["Max"])
+		}
+	}
+}
+
+func TestExportCSV(t *testing.T) {
+	results := map[string]tline{
+		"key1": {count: 1, summ: 10, max: 10, keys: []string{"value1"}},
+		"key2": {count: 2, summ: 20, max: 15, keys: []string{"value2"}},
+	}
+
+	group = "event"
+	groupField = strings.Split(group, ",")
+
+	var buf bytes.Buffer
+	exportCSV(&buf, results)
+
+	expected := "Count,Summ,Max\n1,10,10,value1\n2,20,15,value2\n"
+	if buf.String() != expected {
+		t.Errorf("Ожидалось: %s, получено: %s", expected, buf.String())
+	}
+}
